1ae752dea9e72d30b21aa452331103b074baa431
Josh.Chien <Josh.Chien@hotmail.com>
2019-09-06 19:50:17 +0800

    Reuse dictList

--- MarkerPrinterGUI.py
+++ MarkerPrinterGUI.py
@@ -746,30 +746,7 @@ class MarkerPrinterGUI:
         self.delay = pDelay
         self.displayShape = pDisplayShape
 
-        self.dictList = \
-        [
-            "DICT_4X4_50",
-            "DICT_4X4_100",
-            "DICT_4X4_250",
-            "DICT_4X4_1000",
-            "DICT_5X5_50",
-            "DICT_5X5_100",
-            "DICT_5X5_250",
-            "DICT_5X5_1000",
-            "DICT_6X6_50",
-            "DICT_6X6_100",
-            "DICT_6X6_250",
-            "DICT_6X6_1000",
-            "DICT_7X7_50",
-            "DICT_7X7_100",
-            "DICT_7X7_250",
-            "DICT_7X7_1000",
-            "DICT_ARUCO_ORIGINAL",
-            "DICT_APRILTAG_16h5",
-            "DICT_APRILTAG_25h9",
-            "DICT_APRILTAG_36h10",
-            "DICT_APRILTAG_36h11",
-        ]
+        self.dictList = MarkerPrinter.data["aruco"].keys()
 
         # GUI
         self.window = tk.Tk()
